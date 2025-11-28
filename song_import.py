ec06a78fcb474621621e10986849cbecc325c44d
David Lichti <dlichtistw@gmx.de>
2025-11-28 16:32:19 +0100

    Command Line Interface
    Load argument defaults from a YAML configuration file.

--- song_import.py
+++ song_import.py
@@ -4,15 +4,6 @@ import datetime
 import requests
 import os, sys
 import enum
-import argparse
-
-api_url = None #TODO: Set your ChurchTools API URL here.
-api_token = None #TODO: Set your API token here.
-
-song_category = 0 # Unbekannt
-
-arrangement_name = "SongBeamer"
-arrangement_source = 3 # SongBeamer
 
 class Song:
 
@@ -97,140 +88,6 @@ def collect_pages( session: requests.Session, template: requests.Request ) -> li
 class Ambiguous:
   pass
 
-def match_arrangement( song: Song, arrangements: list[ dict ] ) -> dict | None:
-  for arrangement in arrangements:
-    if arrangement.get( "sourceId" ) == arrangement_source:
-      for file in arrangement.get( "files", [] ):
-        if file.get( "name" ) == os.path.basename( song.file_name ):
-          return arrangement
-
-def match_song( song: Song, candidates: list[ dict ] ) -> dict | Ambiguous | None:
-
-  for s in candidates:
-    if match_arrangement( song, s.get( "arrangements", [] ) ):
-      return s
-
-  if song.ccli:
-    for s in candidates:
-      if s.get( "ccli" ) == song.ccli:
-        return s
-    else:
-      candidates = [ s for s in candidates if "ccli" not in s ]
-
-  if song.author:
-    for s in candidates:
-      if s.get( "author" ) == song.author:
-        return s
-    else:
-      candidates = [ s for s in candidates if "author" not in s ]
-
-  if len( candidates ) == 1:
-    return candidates[ 0 ]
-  elif len( candidates ) > 1:
-    return Ambiguous()
-  else:
-    return None
-  
-def import_song( song: Song, session: requests.Session ) -> dict | None:
-
-  songs = collect_pages( session, requests.Request( "GET", api_url + "/songs", params={ "name": song.title } ) )
-  for s in songs:
-    if "arrangements" not in s:
-      s[ "arrangements" ] = collect_pages( session, requests.Request( "GET", f"{ api_url }/songs/{ s[ "id" ] }/arrangements" ) )
-
-  match match_song( song, songs ):
-    case dict() as existing:
-      print( f"Updating existing song: { existing[ "id" ] } - { existing[ "name" ] }" )
-
-      update = { k: v for k, v in existing.items() if k in [ "name", "author", "ccli", "copyright" ] }
-      update[ "categoryId" ] = existing[ "category" ][ "id" ]
-    
-      if song.ccli:
-        update[ "ccli" ] = song.ccli
-  
-      if song.author:
-        update[ "author" ] = song.author
-
-      if song.copyright:
-        update[ "copyright" ] = song.copyright
-      
-      if result := session.put( f"{ api_url }/songs/{ existing[ "id" ] }", json=update ):
-        return existing
-      else:
-        raise ConnectionError( f"Failed to update song { existing[ "id" ] }: { result.status_code } - { result.text }" )
-  
-    case None:
-      print( f"Creating new song: { song.title }" )
-
-      insert: dict = { "name": song.title, "categoryId": song_category }
-
-      if song.ccli:
-        insert[ "ccli" ] = song.ccli
-      
-      if song.author:
-        insert[ "author" ] = song.author
-
-      if song.copyright:
-        insert[ "copyright" ] = song.copyright
-      
-      if result := session.post( api_url + "/songs", json=insert ):
-        return result.json()[ "data" ]
-      else:
-        raise ConnectionError( f"Faile to create song: { result.status_code } - { result.text }" )
-      
-    case Ambiguous():
-      print( f"Could not match song '{ song.title }'." )
-
-def import_arrangement( song: Song, ct_song: dict, session: requests.Session ) -> dict:
-
-  match match_arrangement( song, ct_song.get( "arrangements", [] ) ):
-    case dict() as existing:
-      print( f"Updating existing arrangement { existing[ "id" ] } for song id { ct_song[ "id" ] }." )
-
-      update = { k: v for k, v in existing.items() if k in [ "beat", "duration", "key", "name", "sourceId", "tempo" ] }
-      update[ "description" ] = f"Updated from '{ os.path.basename( song.file_name ) }' on { datetime.date.today().isoformat() }."
-
-      if song.key:
-        update[ "key" ] = song.key
-
-      if arrangement_source and not update[ "sourceId" ]:
-        update[ "sourceId" ] = arrangement_source
-
-      if result := session.put( f"{ api_url }/songs/{ ct_song[ "id" ] }/arrangements/{ existing[ "id" ] }", json=update ):
-        return existing
-      else:
-        raise ConnectionError( f"Failed to update arrangement { existing[ "id" ] } of song { ct_song[ "id" ] }: { result.status_code } - { result.text }" )
-    
-    case None:
-      print( f"Creating new arrangement for song id { ct_song[ "id" ] }." )
-
-      insert: dict = {
-        "name": arrangement_name,
-        "description": f"Created from '{ os.path.basename( song.file_name ) }' on { datetime.date.today().isoformat() }"
-      }
-
-      if arrangement_source:
-        insert[ "sourceId" ] = arrangement_source
-
-      if song.key:
-        insert[ "key" ] = song.key
-
-      if result := session.post( f"{ api_url }/songs/{ ct_song[ "id" ] }/arrangements", json=insert ):
-        return result.json()[ "data" ]
-      else:
-        raise ConnectionError( f"Failed to create arrangement: { result.status_code } - { result.text }." )
-
-def obtain_csrf_token( session: requests.Session ):
-  if "CSRF-Token" not in session.headers:
-    print( "Requsting CSRF token." )
-    if result := session.get( f"{ api_url }/csrftoken" ):
-      if token := result.json().get( "data" ):
-        session.headers.update( { "CSRF-Token": token } )
-      else:
-        raise ConnectionError( "Failed to obtain CSRF token: No token in response." )
-    else:
-      raise ConnectionError( f"Failed to obtain CSRF token: { result.status_code } - { result.text }" )
-
 class AttachmentMode( enum.Enum ):
   ADD = "add"
   SKIP = "skip"
@@ -239,58 +96,241 @@ class AttachmentMode( enum.Enum ):
   def __str__( self ) -> str:
     return self.value
 
-def import_attachment( song: Song, arrangement: dict, session: requests.Session, mode: AttachmentMode = AttachmentMode.SKIP ):
+class ChurchToolsSession( requests.Session ):
 
-  obtain_csrf_token( session )
+  api_url: str
 
-  if mode != AttachmentMode.ADD:
+  source_id: int | None = None
+  arrangement_name: str = "SongBeamer"
+  song_category: int = 0
 
-    arrangement[ "files" ] = collect_pages( session, requests.Request( "GET", f"{ api_url }/files/song_arrangement/{ arrangement[ "id" ] }" ) )
-  
-    for file in arrangement.get( "files", [] ):
-      if file.get( "name" ) == os.path.basename( song.file_name ):
-        if mode == AttachmentMode.SKIP:
-          print( f"Keeping existing attachment { file[ "id" ] } for arrangement { arrangement[ "id" ] }." )
-          return
-        elif mode == AttachmentMode.REPLACE:
-          print( f"Deleting existing attachment { file[ "id" ] } for arrangement { arrangement[ "id" ] }." )
-          if result := session.delete( f"{ api_url }/files/{ file[ "id" ] }" ):
-            pass
-          else:
-            raise ConnectionError( f"Failed to delete attachment { file[ "id" ] }: { result.status_code } - { result.text }" )
+  def __init__( self, api_url: str, api_token: str ):
+    super().__init__()
+
+    self.api_url = api_url
+
+    self.headers.update( { "Authorization": f"Login { api_token }" } )
+    if result := self.get( f"{ self.api_url }/whoami" ):
+      data = result.json()[ "data" ]
+      print( f"Authenticated as { data[ "firstName" ] } { data[ "lastName" ] } (ID: { data[ "id" ] })." )
+    else:
+      raise ConnectionError( f"Failed to authenticate: { result.status_code } - { result.text }." )
+
+    if result := self.get( f"{ self.api_url }/csrftoken" ):
+      if token := result.json().get( "data" ):
+        self.headers.update( { "CSRF-Token": token } )
+      else:
+        raise ConnectionError( "Failed to obtain CSRF token: No token in response." )
+    else:
+      raise ConnectionError( f"Failed to obtain CSRF token: { result.status_code } - { result.text }" )
+
+  def match_arrangement( self, song: Song, arrangements: list[ dict ] ) -> dict | None:
+    for arrangement in arrangements:
+      if self.source_id is None or arrangement.get( "sourceId" ) == self.source_id:
+        for file in arrangement.get( "files", [] ):
+          if file.get( "name" ) == os.path.basename( song.file_name ):
+            return arrangement
+
+  def match_song( self, song: Song, candidates: list[ dict ] ) -> dict | Ambiguous | None:
+
+    for s in candidates:
+      if self.match_arrangement( song, s.get( "arrangements", [] ) ):
+        return s
+
+    if song.ccli:
+      for s in candidates:
+        if s.get( "ccli" ) == song.ccli:
+          return s
+      else:
+        candidates = [ s for s in candidates if "ccli" not in s ]
+
+    if song.author:
+      for s in candidates:
+        if s.get( "author" ) == song.author:
+          return s
+      else:
+        candidates = [ s for s in candidates if "author" not in s ]
+
+    if len( candidates ) == 1:
+      return candidates[ 0 ]
+    elif len( candidates ) > 1:
+      return Ambiguous()
+    else:
+      return None
     
-  print( f"Uploading attachment '{ os.path.basename( song.file_name ) }' for arrangement { arrangement[ "id" ] }." )
-  with open( song.file_name, "rb" ) as file:
-    files = { "files[]": ( os.path.basename( song.file_name ), file ) }
-    if result := session.post( f"{ api_url }/files/song_arrangement/{ arrangement[ "id" ] }", files=files ):
+  def import_song( self, song: Song ) -> dict | None:
+
+    songs = collect_pages( self, requests.Request( "GET", self.api_url + "/songs", params={ "name": song.title } ) )
+    for s in songs:
+      if "arrangements" not in s:
+        s[ "arrangements" ] = collect_pages( self, requests.Request( "GET", f"{ self.api_url }/songs/{ s[ "id" ] }/arrangements" ) )
+
+    match self.match_song( song, songs ):
+      case dict() as existing:
+        print( f"Updating existing song: { existing[ "id" ] } - { existing[ "name" ] }" )
+
+        update = { k: v for k, v in existing.items() if k in [ "name", "author", "ccli", "copyright" ] }
+        update[ "categoryId" ] = existing[ "category" ][ "id" ]
+      
+        if song.ccli:
+          update[ "ccli" ] = song.ccli
+    
+        if song.author:
+          update[ "author" ] = song.author
+
+        if song.copyright:
+          update[ "copyright" ] = song.copyright
+        
+        if result := self.put( f"{ self.api_url }/songs/{ existing[ "id" ] }", json=update ):
+          return existing
+        else:
+          raise ConnectionError( f"Failed to update song { existing[ "id" ] }: { result.status_code } - { result.text }" )
+    
+      case None:
+        print( f"Creating new song: { song.title }" )
+
+        insert: dict = { "name": song.title, "categoryId": self.song_category }
+
+        if song.ccli:
+          insert[ "ccli" ] = song.ccli
+        
+        if song.author:
+          insert[ "author" ] = song.author
+
+        if song.copyright:
+          insert[ "copyright" ] = song.copyright
+        
+        if result := self.post( self.api_url + "/songs", json=insert ):
+          return result.json()[ "data" ]
+        else:
+          raise ConnectionError( f"Faile to create song: { result.status_code } - { result.text }" )
+        
+      case Ambiguous():
+        print( f"Could not match song '{ song.title }'." )
+
+  def import_arrangement( self, song: Song, ct_song: dict ) -> dict:
+
+    match self.match_arrangement( song, ct_song.get( "arrangements", [] ) ):
+      case dict() as existing:
+        print( f"Updating existing arrangement { existing[ "id" ] } for song id { ct_song[ "id" ] }." )
+
+        update = { k: v for k, v in existing.items() if k in [ "beat", "duration", "key", "name", "sourceId", "tempo" ] }
+        update[ "description" ] = f"Updated from '{ os.path.basename( song.file_name ) }' on { datetime.date.today().isoformat() }."
+
+        if song.key:
+          update[ "key" ] = song.key
+
+        if self.source_id and "sourceId" not in update:
+          update[ "sourceId" ] = self.source_id
+
+        if result := self.put( f"{ self.api_url }/songs/{ ct_song[ "id" ] }/arrangements/{ existing[ "id" ] }", json=update ):
+          return existing
+        else:
+          raise ConnectionError( f"Failed to update arrangement { existing[ "id" ] } of song { ct_song[ "id" ] }: { result.status_code } - { result.text }" )
+      
+      case None:
+        print( f"Creating new arrangement for song id { ct_song[ "id" ] }." )
+
+        insert: dict = {
+          "name": self.arrangement_name,
+          "description": f"Created from '{ os.path.basename( song.file_name ) }' on { datetime.date.today().isoformat() }"
+        }
+
+        if self.source_id:
+          insert[ "sourceId" ] = self.source_id
+
+        if song.key:
+          insert[ "key" ] = song.key
+
+        if result := self.post( f"{ self.api_url }/songs/{ ct_song[ "id" ] }/arrangements", json=insert ):
+          return result.json()[ "data" ]
+        else:
+          raise ConnectionError( f"Failed to create arrangement: { result.status_code } - { result.text }." )
+
+  def import_attachment( self, song: Song, arrangement: dict, mode: AttachmentMode = AttachmentMode.SKIP ):
+
+    if mode != AttachmentMode.ADD:
+
+      arrangement[ "files" ] = collect_pages( self, requests.Request( "GET", f"{ self.api_url }/files/song_arrangement/{ arrangement[ "id" ] }" ) )
+    
+      for file in arrangement.get( "files", [] ):
+        if file.get( "name" ) == os.path.basename( song.file_name ):
+          if mode == AttachmentMode.SKIP:
+            print( f"Keeping existing attachment { file[ "id" ] } for arrangement { arrangement[ "id" ] }." )
+            return
+          elif mode == AttachmentMode.REPLACE:
+            print( f"Deleting existing attachment { file[ "id" ] } for arrangement { arrangement[ "id" ] }." )
+            if result := self.delete( f"{ self.api_url }/files/{ file[ "id" ] }" ):
+              pass
+            else:
+              raise ConnectionError( f"Failed to delete attachment { file[ "id" ] }: { result.status_code } - { result.text }" )
+      
+    print( f"Uploading attachment '{ os.path.basename( song.file_name ) }' for arrangement { arrangement[ "id" ] }." )
+    with open( song.file_name, "rb" ) as file:
+      files = { "files[]": ( os.path.basename( song.file_name ), file ) }
+      if result := self.post( f"{ self.api_url }/files/song_arrangement/{ arrangement[ "id" ] }", files=files ):
+        return
+      else:
+        raise ConnectionError( f"Failed to upload attachment for arrangement { arrangement[ "id" ] }: { result.status_code } - { result.text }" )
+    
+    
+  def set_default_arrangement( self, song_id: int, arrangement_id: int ):
+    if result := self.patch( f"{ self.api_url }/songs/{ song_id }/arrangements/{ arrangement_id }/default" ):
       return
     else:
-      raise ConnectionError( f"Failed to upload attachment for arrangement { arrangement[ "id" ] }: { result.status_code } - { result.text }" )
-  
-  
-def set_default_arrangement( song_id: int, arrangement_id: int, session: requests.Session ):
-  if result := session.patch( f"{ api_url }/songs/{ song_id }/arrangements/{ arrangement_id }/default" ):
-    return
-  else:
-    raise ConnectionError( f"Failed to set arrangement { arrangement_id } as default for song { song_id }: { result.status_code } - { result.text }" )
+      raise ConnectionError( f"Failed to set arrangement { arrangement_id } as default for song { song_id }: { result.status_code } - { result.text }" )
 
 if __name__ == "__main__":
 
-  parser = argparse.ArgumentParser( description="Import .sng song files into ChurchTools." )
-  parser.add_argument( "--attachment-mode", type=AttachmentMode, choices=list( AttachmentMode ), default="skip" )
-  parser.add_argument( "source-directory", type=str, default=".", nargs="?" )
+  import argparse
+  import yaml
+
+  defaults = dict()
+  for path in ( os.path.expanduser( "~/.church-tools.yml" ), ):
+    if os.path.isfile( path ):
+      try:
+        with open( path, "r", ) as config_file:
+          for key, value in yaml.safe_load( config_file ).items():
+            defaults[ key ] = value
+      except FileNotFoundError:
+        pass
+
+  parser = argparse.ArgumentParser( description="Manage a ChurchTools song database." )
+  parser.add_argument( "-u", "--api-url", type=str, help="ChurchTools API URL", required=( "api_url" not in defaults ), metavar="URL" )
+  parser.add_argument( "-t", "--api-token", type=str, help="ChurchTools API token", required=( "api_token" not in defaults ), metavar="TOKEN" )
+
+  sub_parsers = parser.add_subparsers( dest="command" )
+
+  import_parser = sub_parsers.add_parser( "import", help="Import .sng files into ChurchTools." )
+  import_parser.add_argument( "--source-id", type=int, help="Source ID for imported arrangements", metavar="ID" )
+  import_parser.add_argument( "--attachment_mode", type=AttachmentMode, choices=list( AttachmentMode ), default="skip" )
+  import_parser.add_argument( "source-directory", type=str, default=".", nargs="?" )
+
+  test_parser = sub_parsers.add_parser( "test", help="Test the ChurchTools connection." )
+
+  parser.set_defaults( **defaults )
 
   arguments = parser.parse_args()
 
-  with requests.Session() as session:
+  match arguments.command:
 
-    session.headers.update( { "Authorization": f"Login { api_token }" } )
+    case "import":
+      with ChurchToolsSession( arguments.api_url, arguments.api_token ) as session:
 
-    for file in os.scandir():
-      if file.is_file() and file.name.endswith( ".sng" ):
-        if song := read_song( file.path ):
-          if ct_song := import_song( song, session ):
-            if ct_arrangement := import_arrangement( song, ct_song, session ):
-              import_attachment( song, ct_arrangement, session )
-              set_default_arrangement( ct_song[ "id" ], ct_arrangement[ "id" ], session )
+        session.source_id = arguments.get( "source-id" )
 
+        for file in os.scandir( arguments.source_directory ):
+          if file.is_file() and file.name.endswith( ".sng" ):
+            if song := read_song( file.path ):
+              if ct_song := session.import_song( song ):
+                if ct_arrangement := session.import_arrangement( song, ct_song ):
+                  session.import_attachment( song, ct_arrangement )
+                  session.set_default_arrangement( ct_song[ "id" ], ct_arrangement[ "id" ] )
+    
+    case "test":
+      with ChurchToolsSession( arguments.api_url, arguments.api_token ) as session:
+        if result := session.get( f"{ session.api_url }/info" ):
+          info = result.json()
+          print( f"Connected to ChurchTools { info[ "version" ] } of '{ info[ "siteName" ] }'." )
+        else:
+          raise ConnectionError( f"Failed to get ChurchTools info: { result.status_code } - { result.text }" )
