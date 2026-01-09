125c1d54f0ca807fad718ca6431387c716b518cb
David Lichti <dlichtistw@gmx.de>
2026-01-09 20:48:19 +0100

    Schema Sanitizer
    Sanitize data according to a JSON schema, i.e. make them match the schema if possible.

---
+++ schema/sanitize.py
@@ -0,0 +1,48 @@
+import types, typing
+
+type_map: dict[ str, type ] = {
+  "string": str,
+  "null": types.NoneType,
+  "integer": int,
+}
+
+def python_types( type_names: str | typing.Iterable[ str ] ) -> tuple[ type, ... ]:
+  if isinstance( type_names, str ):
+    return python_types( ( type_names, ) )
+  else:
+    return tuple( type_map[ t ] for t in type_names )
+
+def sanitize( value, schema: dict ):
+
+  if any_of := schema.get( "anyOf", [] ):
+    for alternative in any_of:
+      try:
+        return sanitize( value, alternative )
+      except ValueError:
+        pass
+    else:
+      raise ValueError( f"Value '{ value }' does not match any of the allowed schemas." )
+  
+  if types := python_types( schema.get( "type", [] ) ):
+    if not isinstance( value, types ):
+      for t in types:
+        try:
+          value = t( value )
+        except:
+          pass
+        break
+      else:
+        raise ValueError( f"Value '{ value }' does not match any of the allowed types." )
+  
+  if isinstance( value, str ):
+    if lower := schema.get( "minLength" ):
+      if len( value ) < lower:
+        raise ValueError( f"Value '{ value }' is too short." )
+    if upper := schema.get( "maxLength" ):
+      if len( value ) > upper:
+        value = value[ :upper-1 ] + "…"
+    if enums := schema.get( "enum", [] ):
+      if value not in enums:
+        raise ValueError( f"Value '{ value } is not in the list of allowed values." )
+  
+  return value
